2558bcd834ccc6465db870b6aff458fc5087996e
Cola <2681035666@qq.com>
2021-05-08 09:03:53 +0800

    用户管理

--- src/main.js
+++ src/main.js
@@ -7,10 +7,17 @@ import './plugins/element.js'
 import './assets/css/public.css'
 //导入字体图标
 import './assets/fonts/iconfont.css'
-axios.defaults.baseURL='http://127.0.0.1:8888/api/private/v1/'
+//导入axios
 import axios from 'axios'
+//配置求情的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem("token")
+  return config
+})
 
-Vue.prototype.$http=axios
+
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
